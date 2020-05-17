2e37c0cfd1cf8f6c8a9f3b5786b16f079b257a67
MCLEANS <jackagwaya@gmail.com>
2020-05-17 14:02:51 +0300

    delay timer setup

--- src/main.cpp
+++ src/main.cpp
@@ -11,13 +11,30 @@
 
 #include "stm32f4xx.h"
 #include "clockconfig.h"
+#include "ADC.h"
+#include "Timerconfiguration.h"
+
+#define PRESCALER 640
+#define ARR_VALUE 66
+
 
 
 custom_libraries::clock_config system_clock;
+custom_libraries::Timer_configuration delay_timer(TIM3,PRESCALER,ARR_VALUE);
+
+extern "C" void TIM3_IRQHandler(void){
+	if(TIM3->SR & (TIM_SR_UIF)){
+		TIM3->SR &= ~(TIM_SR_UIF);
+	}
+}
 
 int main(void)
 {
 	system_clock.initialize();
+	delay_timer.initialize();
+
+	NVIC_SetPriority(TIM3_IRQn,0x03);
+	NVIC_EnableIRQ(TIM3_IRQn);
 
 	while(1){
 
